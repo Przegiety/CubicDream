8ce758348ec6b72b8aaadaf06fe146b5c9c8046d
Przegiety <t.przegietka@gmail.com>
2018-07-31 00:41:27 +0200

    Initial Commit

---
+++ Assets/Scripts/Game/Coords.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game {
+    struct Coords {
+        public int x, y, z;
+
+        public Coords(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Coords)) {
+                return false;
+            }
+
+            var coords = (Coords)obj;
+            return x == coords.x &&
+                   y == coords.y &&
+                   z == coords.z;
+        }
+
+        public override int GetHashCode() {
+            var hashCode = 373119288;
+            hashCode = hashCode * -1521134295 + x.GetHashCode();
+            hashCode = hashCode * -1521134295 + y.GetHashCode();
+            hashCode = hashCode * -1521134295 + z.GetHashCode();
+            return hashCode;
+        }
+        //TODO: methods
+
+    }
+}
