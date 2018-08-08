7377f89c567bf04480bf91eac70b753e39b37046
Przegiety <t.przegietka@gmail.com>
2018-08-08 22:12:45 +0200

    Added arrow

--- Assets/Scripts/Game/Level.cs
+++ Assets/Scripts/Game/Level.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Game {
-    public class Level {
+    public class Level : MonoBehaviour {
         /// NOTES:
         /// should this be a MonoBehaviour or maybe a scriptableObject.
     }
